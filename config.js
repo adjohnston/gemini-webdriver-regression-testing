78b178ee8bdb38b076d399e9af8c0766dd965a11
Adam Johnston <adam.johnston3679@gmail.com>
2016-02-02 20:27:41 +0000

    add a config to handle responsive viewports

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  viewports: {
+    small: {
+      width: 320,
+      height: 480
+    },
+
+    medium: {
+      width: 768,
+      height: 1024
+    },
+
+    large: {
+      width: 1280,
+      height: 950
+    }
+  }
+}
